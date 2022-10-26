11443e372e25fe7b0eba23dea5ea46c55faec592
daehyeokyou <ydh4085@naver.com>
2022-10-26 07:49:09 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/ydh-training/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = "network"
+}
